b1c6b59342671973a8a84e2c9e2f1cf0a9f33763
aakash <aakash_conky@hotmail.com>
2023-10-22 23:20:06 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ const App = () => {
       <button onClick={()=>setNumber(number+1)} className="button">Add</button>
       <button onClick={()=>setNumber(number-1)} className="button">Less</button>
       <button onClick={()=>setNumber(9)} className="button">Reset</button>
+      <br></br>
+      <button onClick={()=>setNumber(number*2)} className="button">Double it</button>
+      <br></br>
+      <button onClick={()=>setNumber(number*100)} className="button">Jackpot</button>
+
     </div>
 }
 
